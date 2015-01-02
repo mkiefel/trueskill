c9f742341c352af0267383b4911680d437389e20
Martin Kiefel <mrtrac@nopw.de>
2015-01-02 15:46:30 +0100

    Implements learned knob parameter file.

---
+++ Parameter.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
+module Parameter ( Knobs(..)
+                 , readKnobs
+                 , writeKnobs
+                 ) where
+
+import           Data.Aeson
+import           Data.Aeson.TH
+import qualified Data.ByteString.Lazy as BL
+
+data Knobs = Knobs
+             { getDefaultMuOffense :: Double
+             , getDefaultSigmaOffense :: Double
+             , getDefaultMuDefense :: Double
+             , getDefaultSigmaDefense :: Double
+             , getSigmaOffense :: Double
+             , getSigmaDefense :: Double
+             }
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 3} ''Knobs)
+
+readKnobs :: FilePath -> IO (Maybe Knobs)
+readKnobs path = BL.readFile path >>= return . decode
+
+writeKnobs :: FilePath -> Knobs -> IO ()
+writeKnobs path knobs = BL.writeFile path $ encode knobs
--- train_app.hs
+++ train_app.hs
@@ -7,119 +7,80 @@ import           Control.Monad.Trans.Either ( EitherT(..)
                                             , hoistEither )
 import           Control.Monad.Trans.Class ( lift )
 import           Control.Parallel.Strategies
-import           Data.Default ( def )
-import           Linear.V2
+import qualified Data.Vector as V
+import           Linear.V2 hiding ( trace )
 
-import           TrueSkill ( Parameter(..)
-                           , Player
-                           , skills
-                           , makeSkills
-                           , fromMuSigma2
-                           , sigmaOffense
-                           , sigmaDefense
-                           )
 import           TrueSkill.Autodiff hiding ( lift )
 
+import           Parameter
 import           Types
 import           Train
 
--- | Default player skill mean.
-defaultMuOffense :: Floating d => d
-defaultMuOffense = 3.0 / 11.0
+import           Debug.Trace
 
--- | Default player skill standard deviation.
-defaultSigmaOffense :: Floating d => d
-defaultSigmaOffense = 0.1
-
--- | Default player skill mean.
-defaultMuDefense :: Floating d => d
-defaultMuDefense = 1.0 / 11.0
-
--- | Default player skill standard deviation.
-defaultSigmaDefense :: Floating d => d
-defaultSigmaDefense = 0.1
-
-defaultParameter :: Floating d => Parameter d
-defaultParameter = Parameter
-    { _sigmaOffense = defaultSigmaOffense / 5.0
-    , _sigmaDefense = defaultSigmaDefense / 5.0
-    }
-
-defaultPlayer :: Floating d => Player d
-defaultPlayer = skills .~ makeSkills
-                (fromMuSigma2 defaultMuOffense (defaultSigmaOffense**2))
-                (fromMuSigma2 defaultMuDefense (defaultSigmaDefense**2))
-                $ def
-
+parallelObjectiveGrad :: V.Vector Game -> V.Vector Game
+                      -> [Double] -> [Double]
 parallelObjectiveGrad trainData valData parameter = runEval $ do
   let offsets = [ 0, 2, 4 ]
 
-  let adParameter = map (liftParameter parameter) offsets
+  let adParameter = map liftParameter offsets
   values <- mapM (rpar . objective trainData valData) adParameter
   mapM_ rseq values
 
-  return $ concatMap (readGradient . getGradient) values
+  let gradient = concatMap (readGradient . getGradient) values
+
+  return $ trace ("snorm: " ++ show (snorm gradient)) gradient
     where
+      snorm gradient = sum $ map (^2) gradient
+
       readGradient v = [ v^._x, v^._y ]
-      liftParameter parameter offset = map go $ zip [0..] parameter
+      liftParameter offset = map go $ zip [0..] parameter
         where
           go (i, p) = makeAD p (i - offset)
 
+train trainFile valFile outKnobsFile knobs = runEitherT $ do
+  trainData <- hoistEither =<<
+               (lift $ readGamesFromCsv trainFile)
+  valData <- hoistEither =<<
+             (lift $ readGamesFromCsv valFile)
+
+  let ps = take 200 $ optimizer (objective trainData valData)
+           (parallelObjectiveGrad trainData valData)
+           [ getSigmaOffense knobs
+           , getSigmaDefense knobs
+           , getDefaultMuOffense knobs
+           , getDefaultSigmaOffense knobs ^ (2 :: Int)
+           , getDefaultMuDefense knobs
+           , getDefaultSigmaDefense knobs ^ (2 :: Int)
+           ]
+
+  lift $ print ps
+
+  let [ sigmaOffense'
+        , sigmaDefense'
+        , defaultMuOffense'
+        , defaultSigmaOffense2'
+        , defaultMuDefense'
+        , defaultSigmaDefense2' ] = last ps
+
+  lift $ writeKnobs outKnobsFile $
+    Knobs defaultMuOffense' (sqrt defaultSigmaOffense2')
+    defaultMuDefense' (sqrt defaultSigmaDefense2')
+    sigmaOffense' sigmaDefense'
+
+  return 0
+
+main :: IO ()
 main = do
-    [trainFile, valFile, testFile] <- getArgs
+  [trainFile, valFile, inKnobsFile, outKnobsFile] <- getArgs
 
-    results <- runEitherT $ do
-      trainData <- hoistEither =<<
-                   (lift $ readGamesFromCsv trainFile)
-      valData <- hoistEither =<<
-                 (lift $ readGamesFromCsv valFile)
+  rawKnobs <- readKnobs inKnobsFile
 
-      let ps = take 20 $ optimizer (objective trainData valData)
-               (parallelObjectiveGrad trainData valData)
-               [ defaultParameter ^. sigmaOffense
-               , defaultParameter ^. sigmaDefense
-               , defaultMuOffense
-               , defaultSigmaOffense**2
-               , defaultMuDefense
-               , defaultSigmaDefense**2
-               ]
-
-      lift $ print ps
-
-      let [ sigmaOffense'
-            , sigmaDefense'
-            , muOffense'
-            , sigmaOffense2'
-            , muDefense'
-            , sigmaDefense2' ] = last ps
-
-      let trainedParameter = Parameter
-                             { _sigmaOffense = sigmaOffense'
-                             , _sigmaDefense = sigmaDefense' }
-
-      let trainedPlayer = skills .~ makeSkills
-                          (fromMuSigma2 muOffense' sigmaOffense2')
-                          (fromMuSigma2 muDefense' sigmaDefense2')
-                          $ def
-
-      testData <- hoistEither =<<
-                  (lift $ readGamesFromCsv trainFile)
-
-      let model = trainModel trainedParameter trainedPlayer trainData
-                  :: Model Double
-
-      {-let (best, value) = M.foldrWithKey-}
-      {-findBestPlayer ("noland", -100) model-}
-      {-lift $ putStrLn $ best ++ ": " ++ show value-}
-
-      {-let prediction = testModel trainedParameter trainedPlayer model-}
-      {-testData-}
-
-      {-return $ V.length-}
-      {-$ V.filter (\(g, p) -> (g ^. result) == p)-}
-      {-$ V.zip testData prediction-}
-      return 0
-    report results
+  case rawKnobs of
+    Just knobs ->
+      train trainFile valFile outKnobsFile knobs >>= report
+    Nothing ->
+      putStrLn "error reading knobs file"
 
   where
     report (Left err)      = putStrLn err
